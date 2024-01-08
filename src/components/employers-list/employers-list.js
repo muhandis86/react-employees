3b0cbf84bc6cd415e4c49320f7a22d432bae7f60
zaur <eskendarov@gmail.com>
2024-01-08 17:45:14 +0300

    add 'key' in props for optimization

--- src/components/employers-list/employers-list.js
+++ src/components/employers-list/employers-list.js
@@ -5,8 +5,9 @@ import './employers-list.css'
 const EmployersList = ({ data }) => {
 
     const elements = data.map(item => {
+        const { id, ...itemProps } = item;
         return (
-            <EmployersListItem name={item.name} salary={item.salary} increase={item.increase} /> // {...item}
+            <EmployersListItem key={id} {...itemProps} />
         )
     })
 

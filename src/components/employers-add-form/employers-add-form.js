7c2137502cd492dc355290c5b38eb52cade63e3b
zaur <eskendarov@gmail.com>
2024-01-12 00:00:58 +0300

    input validation fo add-form (my home work)

--- src/components/employers-add-form/employers-add-form.js
+++ src/components/employers-add-form/employers-add-form.js
@@ -4,29 +4,47 @@ import './employers-add-form.css';
 class EmployersAddForm extends Component {
     constructor(props) {
         super(props);
+        this.inputClasses = "form-control new-post-label";
         this.state = {
             name: '',
-            salary: ''
+            salary: '',
+            nameClasses: this.inputClasses,
+            salaryClasses: this.inputClasses
         }
     }
 
     onValueChange = (e) => {
         this.setState({
-            [e.target.name]: e.target.value
+            [e.target.name]: e.target.value,
+            nameClasses: e.target.name === 'name' ? this.inputClasses : this.state.nameClasses,
+            salaryClasses: e.target.name === 'salary' ? this.inputClasses : this.state.salaryClasses
+
         })
     }
 
     onSubmit = (e) => {
         e.preventDefault();
-        this.props.onAdd(this.state.name, this.state.salary);
-        this.setState({
-            name: '',
-            salary: ''
-        })
+        const isInputNameEmpty = this.state.name.length < 3;
+        const isInputSalaryEmpty = !this.state.salary;
+
+        if (isInputNameEmpty || isInputSalaryEmpty) {
+            this.setState({
+                nameClasses: isInputNameEmpty ? this.state.nameClasses + ' is-invalid' : this.state.nameClasses,
+                salaryClasses: isInputSalaryEmpty ? this.state.salaryClasses + ' is-invalid' : this.state.salaryClasses
+            })
+        } else {
+            this.props.onAdd(this.state.name, this.state.salary);
+            this.setState({
+                name: '',
+                salary: '',
+                nameClasses: this.inputClasses,
+                salaryClasses: this.inputClasses
+            })
+        }
     }
 
     render() {
-        const { name, salary } = this.state;
+        const { name, salary, nameClasses, salaryClasses } = this.state;
 
         return (
             <div className="app-add-form">
@@ -35,13 +53,13 @@ class EmployersAddForm extends Component {
                     className="add-form d-flex"
                     onSubmit={this.onSubmit}>
                     <input type="text"
-                        className="form-control new-post-label"
+                        className={nameClasses}
                         placeholder="Как его зовут?"
                         name="name"
                         value={name}
                         onChange={this.onValueChange} />
                     <input type="number"
-                        className="form-control new-post-label"
+                        className={salaryClasses}
                         placeholder="З/П в $?"
                         name="salary"
                         value={salary}
